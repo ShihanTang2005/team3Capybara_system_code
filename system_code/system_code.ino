1b45ba80a09c8a56d076c304541bb005f2a87ba9
HeonyoungChoi <iamhun0@unist.ac.kr>
2025-01-24 03:48:20 +0900

    Update system_code.ino

--- system_code/system_code.ino
+++ system_code/system_code.ino
@@ -46,29 +46,6 @@ int delay_time = 500; //ms
 
 ///// XBee initialization function////
 //////////////////////////////////////
-void xbee_setup() {
-  Serial1.begin(9600); 
-  Serial1.println("XBee initialized"); 
-}
-
-void xbee_send_data() {
-  // Collect sensor data
-  String gpsData = GPS_get_data();       
-  float pressure = bmp280_readPressure(); 
-  float temperature = temp_sensor_read(); 
-  float accelX = mpu_getAccelX();      
-
-  // Format data into a CSV string
-  String dataPacket = String("Time: ") + millis() + ", ";
-  dataPacket += "GPS: " + gpsData + ", ";
-  dataPacket += "Pressure: " + String(pressure, 2) + ", ";
-  dataPacket += "AccelX: " + String(accelX, 2) + ", ";
-  dataPacket += "Temp: " + String(temperature, 2);
-
-  // Send the formatted data via XBee
-  Serial1.println(dataPacket); 
-  Serial.println(dataPacket);  
-
 
 void setup() {
   Serial.begin(9600);
@@ -132,7 +109,7 @@ void sensor_loop(){
   mpu_loop();
   bmp280_loop();
   GPS_loop();
-  xbee_send_data();
+  xbee_loop();
 }
 
 
